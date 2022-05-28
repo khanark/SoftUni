de389f6258684a74ee7f66257909ed1fadb26f63
JScodeKiddie <JScodeKiddie@gmail.com>
2022-05-28 19:59:17 +0200

    fixed arena.js

--- Fundamentals/Associative Arrays - Exercise/arena.js
+++ Fundamentals/Associative Arrays - Exercise/arena.js
@@ -1,96 +1,79 @@
 function gladiator(arr) {
-	// create collection that stores gladiator, technique and his skill
-	let gladiators = {};
-	let lastSkillName = '';
-	// parse the input (gladiator nad technique are strings, skills are numbers)
-	while (arr[0] !== 'Ave Cesar') {
-		if (arr[0].includes(' vs ')) break;
-		let line = arr.shift();
-		const [gladiator, skillName, skillPower] = line.split(' -> ');
+	// create collection
+	let tier = {};
+	let lastValue = 0;
+	// parse the input
+	for (const line of arr) {
+		if (line === 'Ave Cesar') break;
+		if (line.includes(' vs ')) {
+			const [gladiator1, gladiator2] = line.split(' vs ');
+			if (tier.hasOwnProperty(gladiator1) && tier.hasOwnProperty(gladiator2)) {
+				const skills = Object.keys(tier[gladiator1]);
 
-		if (!gladiators.hasOwnProperty(gladiator)) {
-			gladiators[gladiator] = new Map();
-			gladiators[gladiator].set(skillName, Number(skillPower));
-			lastSkillName = skillName;
-			// - if the gladiator is already present only add his new technique or update his skill
-		} else {
-			let lastSkillPower = gladiators[gladiator].get(lastSkillName);
-			if (lastSkillPower <= Number(skillPower)) {
-				gladiators[gladiator].set(skillName, Number(skillPower));
-				lastSkillName = skillName;
-			}
-		}
-	}
-	// parse the input when having command = "${gladiator} vs ${gladiator}" type
-	while (arr[0] !== 'Ave Cesar') {
-		// check if the both gladiators exist in the object
-		let line = arr.shift();
-		const [firstGladiator, secondGladiator] = line.split(' vs ');
-		if (
-			gladiators.hasOwnProperty(firstGladiator) &&
-			gladiators.hasOwnProperty(secondGladiator)
-		) {
-			const techniques = gladiators[firstGladiator].keys();
-			const firstGladiatorTotal = Array.from(
-				gladiators[firstGladiator].values()
-			).reduce((a, b) => a + b);
-			const secondGladiatorTotal = Array.from(
-				gladiators[secondGladiator].values()
-			).reduce((a, b) => a + b);
-			// Compare their techniques, if they got at least one in common, the gladiator with better total skill points wins and the other is demoted from the tier -> remove him.
-			for (const technique of techniques) {
-				if (gladiators[secondGladiator].has(technique)) {
-					if (firstGladiatorTotal > secondGladiatorTotal) {
-						delete gladiators[secondGladiator];
-					} else {
-						delete gladiators[firstGladiator];
+				for (const skill of skills) {
+					if (tier[gladiator2].hasOwnProperty(skill)) {
+						const totalGladiator1 = Object.values(tier[gladiator1]).reduce(
+							(a, b) => a + b
+						);
+						const totalGladiator2 = Object.values(tier[gladiator2]).reduce(
+							(a, b) => a + b
+						);
+
+						if (totalGladiator1 > totalGladiator2) {
+							delete tier[gladiator2];
+						} else {
+							delete tier[gladiator1];
+						}
+						break;
 					}
 				}
 			}
-		}
-	}
-	// print the output
-	const result = Object.entries(gladiators).sort(function (a, b) {
-		let firstGlad = Array.from(a[1].values()).reduce((a, b) => a + b);
-		let secondGlad = Array.from(b[1].values()).reduce((a, b) => a + b);
-	if (secondGlad - firstGlad == 0) {
-		return a[0].localeCompare(b[0])
-	} else {
-		return secondGlad - firstGlad
-	}
-	});
-	for (const [gladiator, values] of result) {
-		const totalPoints = Array.from(values.values()).reduce((a, b) => a + b);
-		const sorted = Array.from(values.entries()).sort((a, b) => {
-			if (b[1] - a[1] == 0) {
-				return a[0].localeCompare(b[0]);
-			} else {
-				return b[1] - a[1];
+		} else {
+			const [gladiator, skill, power] = line.split(' -> ');
+
+			if (!tier.hasOwnProperty(gladiator)) {
+				tier[gladiator] = { [skill]: 0 };
+				lastValue = 0;
 			}
-		});
-		console.log(`${gladiator}: ${totalPoints} skill`);
-		for (const [skillName, value] of sorted) {
-			console.log(`- ${skillName} <!> ${value}`);
+			if (tier[gladiator].hasOwnProperty(skill)) {
+				if (lastValue <= Number(power)) {
+					tier[gladiator][skill] = Number(power);
+				}
+				lastValue = tier[gladiator][skill];
+			} else {
+				tier[gladiator][skill] = Number(power);
+			}
 		}
+	}
+
+	const sorted = Object.entries(tier).sort((a, b) => {
+		const totalGladiator1 = Object.values(a[1]).reduce((a, b) => a + b);
+		const totalGladiator2 = Object.values(b[1]).reduce((a, b) => a + b);
+		return totalGladiator2 - totalGladiator1;
+	});
+
+	for (const [name, obj] of sorted) {
+		const totalPoints = Object.values(obj).reduce((a, b) => a + b);
+		console.log(`${name}: ${totalPoints} skill`);
+
+		const sortedBySkillPower = Object.entries(obj).sort(
+			(a, b) => b[1] - a[1] || a[0].localeCompare(b[0])
+		);
+		sortedBySkillPower.forEach(skill =>
+			console.log(`- ${skill[0]} <!> ${skill[1]}`)
+		);
 	}
 }
-//TODO: Fix the problem, Judge gives you 70/100
-// gladiator([
-// 	'Peter -> BattleCry -> 400',
-// 	'Alex -> PowerPunch -> 300',
-// 	'Stefan -> Duck -> 200',
-// 	'Stefan -> Tiger -> 100',
-// 	'Ave Cesar',
-// ]);
-console.log('-----');
+
 gladiator([
-	'Peter -> Duck -> 400',
-	'Julius -> Shield -> 150',
-	'Gladius -> Heal -> 200',
-	'Gladius -> Support -> 250',
-	'Gladius -> Shield -> 250',
-	'Peter vs Gladius',
+	'Peter -> Duck',
+	'Julius -> Shield -> 0',
+	'Gladius -> Heal -> 400',
+	'Gladius -> Heal -> 600',
+	'Gladius -> Shield -> 0',
+	'Gladius -> Shield -> 0',
 	'Gladius vs Julius',
+	'Peter vs Gladius',
 	'Gladius vs Maximilian',
 	'Ave Cesar',
 ]);
