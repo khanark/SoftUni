09970aa8c6a3bc9479077681c50c22fff24bfc2f
JScodeKiddie <JScodeKiddie@gmail.com>
2022-06-08 17:23:40 +0200

    added computerStore.js

---
+++ Fundamentals/Fundamentals - Exams/01. Programming Fundamentals Mid Exam Retake/computerStore.js
@@ -0,0 +1,40 @@
+function printReceipt(arr) {
+	let totalPrice = 0;
+	const typeOfCustomer = arr.pop();
+
+	for (const price of arr) {
+		if (price < 0) {
+			console.log('Invalid price!');
+			continue;
+		}
+		totalPrice += Number(price);
+	}
+
+	if (totalPrice > 0) {
+		const taxes = totalPrice * 0.2;
+		let priceWithTaxes = totalPrice + taxes;
+
+		if (typeOfCustomer === 'special') {
+			priceWithTaxes *= 0.9;
+		}
+
+		console.log("Congratulations you've just bought a new computer!");
+		console.log(`Price without taxes: ${totalPrice.toFixed(2)}$`);
+		console.log(`Taxes: ${taxes.toFixed(2)}$`);
+		console.log('-----------');
+		console.log(`Total price: ${priceWithTaxes.toFixed(2)}$`);
+	} else {
+		console.log('Invalid order!');
+	}
+}
+printReceipt([
+	'1023',
+	'15',
+	'-20',
+	'-5.50',
+	'450',
+	'20',
+	'17.66',
+	'19.30',
+	'regular',
+]);
