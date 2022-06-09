60ac1bf1f19482fbfd8b14e5f5b70f9b63ff3902
JScodeKiddie <JScodeKiddie@gmail.com>
2022-06-09 22:04:13 +0200

    added comments for computerStore.js for better readability

--- Fundamentals/Fundamentals - Exams/01. Programming Fundamentals Mid Exam Retake/computerStore.js
+++ Fundamentals/Fundamentals - Exams/01. Programming Fundamentals Mid Exam Retake/computerStore.js
@@ -1,7 +1,10 @@
 function printReceipt(arr) {
+	// create init values
 	let totalPrice = 0;
 	const typeOfCustomer = arr.pop();
 
+
+	// parse the input
 	for (const price of arr) {
 		if (price < 0) {
 			console.log('Invalid price!');
@@ -10,6 +13,7 @@ function printReceipt(arr) {
 		totalPrice += Number(price);
 	}
 
+	// check if total price > 0
 	if (totalPrice > 0) {
 		const taxes = totalPrice * 0.2;
 		let priceWithTaxes = totalPrice + taxes;
@@ -18,6 +22,7 @@ function printReceipt(arr) {
 			priceWithTaxes *= 0.9;
 		}
 
+		// print the output
 		console.log("Congratulations you've just bought a new computer!");
 		console.log(`Price without taxes: ${totalPrice.toFixed(2)}$`);
 		console.log(`Taxes: ${taxes.toFixed(2)}$`);
