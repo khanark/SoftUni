cd22bb02f11b58b1b6ef76a9339694f4962ee1b3
JScodeKiddie <JScodeKiddie@gmail.com>
2022-06-19 17:34:18 +0200

    fixed buildAWall.js

--- Fundamentals/Arrays Advanced - Excercise/buildAWall.js
+++ Fundamentals/Arrays Advanced - Excercise/buildAWall.js
@@ -1,30 +1,21 @@
-function buildWall(numbers) {
-	let concreteSum = 0;
+function buildWall(input) {
+	let pesos = 0;
 	const concreteEachDay = [];
 
-	while (numbers.length > 0) {
-		let flag = false;
-		numbers.map((crew, index, array) => {
-			if (crew < 30) {
-				numbers[index] = crew + 1;
-				concreteSum += 195;
-				flag = true;
-			} else {
-				numbers.splice(index, 1);
-				crew = array[index - 1];
+	while (input.length != 0) {
+		let crews = input.length;
+		for (let i = 0; i < input.length; i++) {
+			input[i]++;
+			pesos += 195 * 1900;
+
+			if (input[i] == 30) {
+				input.splice(input.indexOf(input[i]), 1);
+				i = i - 1;
 			}
-		});
-
-		if (flag) {
-			concreteEachDay.push(concreteSum);
-			concreteSum = 0;
 		}
+		concreteEachDay.push(195 * crews);
 	}
-
-	const wallCost = concreteEachDay.reduce((a, b) => a + b) * 1900;
-
 	console.log(concreteEachDay.join(', '));
-	console.log(`${wallCost} pesos`);
+	console.log(pesos + ' pesos');
 }
-
-buildWall([17, 22, 17, 19, 17]);
+buildWall([21, 25, 28]);
