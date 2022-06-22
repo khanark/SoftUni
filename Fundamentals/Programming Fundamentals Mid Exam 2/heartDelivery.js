da8f737208e6b76104b71b289ab4f2105c0a4c79
JScodeKiddie <JScodeKiddie@gmail.com>
2022-06-22 20:54:01 +0200

    fixed heartDelivery.js

--- Fundamentals/Programming Fundamentals Mid Exam 2/heartDelivery.js
+++ Fundamentals/Programming Fundamentals Mid Exam 2/heartDelivery.js
@@ -1,44 +1,47 @@
 function deliver(arr) {
-	// parse the input
-	let cupid = 0;
-	const neighborhood = arr.shift().split('@').map(Number);
+	// create the initial array
+	const houses = arr.shift().split('@').map(Number);
+	let jump = 0;
 
-	// loop the commands
-	for (const line of arr) {
-		if (line === 'Love!') break;
+	// loop the input
+	while (arr[0] !== 'Love!') {
+		let index = arr.shift().split(" ")[1]
+		index = Number(index);
+		jump += index;
 
-		const jumpLength = Number(line.slice(-1));
-		let cupidsJump = cupid + jumpLength;
-
-		// check  if the jump is out of boundaries if so start the logic again else continue
-		cupidsJump >= neighborhood.length ? StartAgain(Jump) : Jump();
-
-		function Jump() {
-			if (neighborhood[cupidsJump] === 0) {
-				console.log(`Place ${cupidsJump} already had Valentine's day.`);
+			if (jump >= houses.length) {
+				jump = 0;
 			}
-			if (neighborhood[cupidsJump] > 0) {
-				neighborhood[cupidsJump] -= 2;
-				if (neighborhood[cupidsJump] === 0) {
-					console.log(`Place ${cupidsJump} has Valentine's day.`);
+			if (houses[jump] == 0) {
+				console.log(`Place ${jump} already had Valentine's day.`);
+			} else {
+				houses[jump] -= 2;
+				if (houses[jump] == 0) {
+					console.log(`Place ${jump} has Valentine's day.`);
 				}
 			}
-			cupid = cupidsJump;
-		}
-
-		function StartAgain(func) {
-			cupidsJump = 0;
-			return func();
-		}
 	}
 
-	// print the output
-	console.log(`Cupid's last position was ${cupid}.`);
-	if (neighborhood.every(house => house === 0)) {
-		console.log('Mission was successful.');
-	} else {
-		const failedPlaces = neighborhood.filter(place => place !== 0).length;
+	//print the output
+	console.log(`Cupid's last position was ${jump}.`);
+	const isFailed = houses.filter(el => el != 0).length > 0;
+
+	if (isFailed) {
+		const failedPlaces = houses.filter(el => el != 0).length;
 		console.log(`Cupid has failed ${failedPlaces} places.`);
+	} else {
+		console.log('Mission was successful.');
 	}
 }
-deliver(['0', 'Jump 1', 'Jump 2', 'Love!']);
+deliver([
+	'2@2@2@2@2@2@2@2@2',
+	'Jump 2',
+	'Jump 2',
+	'Jump 2',
+	'Jump 2',
+	'Jump 2',
+	'Jump 2',
+	'Jump 2',
+	'Jump 1',
+	'Love!',
+]);
