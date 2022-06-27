fb746923f2cc2fde883a63063522ad94a8490c08
JScodeKiddie <JScodeKiddie@gmail.com>
2022-06-27 20:30:09 +0200

    added dayOfWeek.js

---
+++ Advanced/Syntax, Functions and Statements - Lab/dayOfWeek.js
@@ -0,0 +1,10 @@
+function printNumber(string) {
+    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+    if (days.includes(string)) {
+        console.log(days.indexOf(string) + 1);
+    } else {
+        console.log("error");
+    }
+}
+printNumber("Friday")
