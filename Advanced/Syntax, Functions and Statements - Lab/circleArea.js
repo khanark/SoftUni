06e8bf3eaa503af7b946e63a7b8422488ff64137
JScodeKiddie <JScodeKiddie@gmail.com>
2022-06-27 20:17:50 +0200

    added circleArea.js

---
+++ Advanced/Syntax, Functions and Statements - Lab/circleArea.js
@@ -0,0 +1,10 @@
+function circle(input) {
+	if (typeof input == 'number') {
+		console.log((Math.PI * input ** 2).toFixed(2));
+	} else {
+		console.log(
+			`We can not calculate the circle area, because we receive a ${typeof input}.`
+		);
+	}
+}
+circle(5);
