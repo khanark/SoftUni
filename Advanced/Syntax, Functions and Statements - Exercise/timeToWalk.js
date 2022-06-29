9478cab3fcf9c6b312230c67c493919a2d9a7f50
JScodeKiddie <JScodeKiddie@gmail.com>
2022-06-29 19:11:21 +0200

    added timeToWalk.js

---
+++ Advanced/Syntax, Functions and Statements - Exercise/timeToWalk.js
@@ -0,0 +1,18 @@
+function calcWalk(steps, footPrintsMeters, speed) {
+    let stepsNumber = Number(steps);
+    let stepsMetersHr = Number(footPrintsMeters);
+    let studentSpeed = Number(speed);
+
+    let distanceMeters = stepsNumber * stepsMetersHr;
+    let speedMetersSec = studentSpeed / 3.6;
+    let time = distanceMeters / speedMetersSec;
+    let rest = Math.floor(distanceMeters / 500);
+
+    let timeMin = Math.floor(time / 60);
+    let timeSec = Math.round(time - (timeMin * 60));
+    let timeHr = Math.floor(time / 3600);
+
+    console.log((timeHr < 10 ? "0" : "") + timeHr + ":" + (timeMin + rest < 10 ? "0" : "") + (timeMin + rest) + ":" + (timeSec < 10 ? "0" : "") + timeSec);
+}
+
+calcWalk(2564, 0.70, 5.5)
