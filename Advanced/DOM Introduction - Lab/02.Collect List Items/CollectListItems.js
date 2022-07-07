b2597d599dbeb8360d77ad8391f4ed46aec5720c
JScodeKiddie <JScodeKiddie@gmail.com>
2022-07-07 20:34:38 +0200

    updated Collect List Items Problem

--- Advanced/DOM Introduction - Lab/02.Collect List Items/CollectListItems.js
+++ Advanced/DOM Introduction - Lab/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,14 @@
 function extractText() {
-    // TODO
+    // get the text area element
+    const textArea = document.getElementById("result")
+
+    // get the list of the items
+    const items = document.querySelectorAll("li")
+
+    // store the the values of the items elements
+    const result = [...items].map(el => el.textContent)
+    console.log(result);
+
+    // push the result back in the text Area
+    textArea.textContent = result.join("\n");
 }
