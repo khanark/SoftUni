242124488ebe34c2d0bb0ffc2a43ed6c71a6133c
JScodeKiddie <JScodeKiddie@gmail.com>
2022-07-07 20:43:25 +0200

    updated Sum Numbers Problem

--- Advanced/DOM Introduction - Lab/03.Sum Numbers/SumNumbers.js
+++ Advanced/DOM Introduction - Lab/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,13 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    // take the first element input
+    const firstNumber = document.getElementById("num1").value
+
+    // take the second element input
+    const secondNumber = document.getElementById("num2").value
+
+    // get the text element
+    const text = document.getElementById("sum")
+
+    // store the result
+    text.value = Number(firstNumber)  + Number(secondNumber)
 }
