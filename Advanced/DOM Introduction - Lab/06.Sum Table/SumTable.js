4c8249ccc73b6d09551dded47bac0df11e8d1056
JScodeKiddie <JScodeKiddie@gmail.com>
2022-07-07 21:30:09 +0200

    updated Sum Table Problem

--- Advanced/DOM Introduction - Lab/06.Sum Table/SumTable.js
+++ Advanced/DOM Introduction - Lab/06.Sum Table/SumTable.js
@@ -1,3 +1,10 @@
 function sumTable() {
+	// store all the product prices
+	const prices = [...document.querySelectorAll('tr td:last-child')].slice(0, -1);
 
+    // get the total sum of all product's prices
+	const sum  = prices.reduce((sum, value) => sum + Number(value.textContent), 0)
+
+    // render the total sum of all the products
+    document.getElementById("sum").textContent = sum
 }
