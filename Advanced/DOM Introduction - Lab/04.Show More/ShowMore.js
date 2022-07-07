7fa5df6e997e9d44d44df9e88b519b826c753abe
JScodeKiddie <JScodeKiddie@gmail.com>
2022-07-07 20:50:02 +0200

    updated Show More Problem

--- Advanced/DOM Introduction - Lab/04.Show More/ShowMore.js
+++ Advanced/DOM Introduction - Lab/04.Show More/ShowMore.js
@@ -1,3 +1,13 @@
 function showText() {
-    // TODO
-}
+    // get the element that twe need to show
+    const textToShow = document.getElementById("text")
+
+    // get the more (link) element
+    const show = document.getElementById("more")
+
+    // change the link's style -> display property to none when clicked
+    show.style.display = "none"
+
+    // change the new text style -> display property to inline in order to be shown when the link is clicked
+    textToShow.style.display = "inline"
+ }
