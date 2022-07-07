3c5d3caa2d4af81b9a119d5e52378b27a7eb08fb
JScodeKiddie <JScodeKiddie@gmail.com>
2022-07-07 21:05:42 +0200

    updated Colorize Table Problem

--- Advanced/DOM Introduction - Lab/05.Colorize Table/ColorizeTable.js
+++ Advanced/DOM Introduction - Lab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,6 @@
 function colorize() {
-    // TODO
+	// god mode one liner, I feel sorry for debugging sessions
+	[...document.querySelectorAll('table tr:nth-child(even)')].forEach(
+		el => (el.style.backgroundColor = 'Teal')
+	);
 }
