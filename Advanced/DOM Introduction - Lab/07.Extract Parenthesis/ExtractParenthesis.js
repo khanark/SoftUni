d1114381d36fa49560658d916602b6def5c8e6b2
JScodeKiddie <JScodeKiddie@gmail.com>
2022-07-07 21:42:14 +0200

    updated Extract Parenthesis Problem

--- Advanced/DOM Introduction - Lab/07.Extract Parenthesis/ExtractParenthesis.js
+++ Advanced/DOM Introduction - Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,22 @@
 function extract(content) {
+    // take the text element
+    const text = document.getElementById(content).textContent
 
+    // create the RegExp pattern
+    const pattern = /\((.+?)\)/g
+
+    // match the pattern to the content
+    let match = pattern.exec(text)
+
+    // create collection
+    const result = []
+
+    // store all the valid values
+    while(match) {
+        result.push(match[1])
+        match = pattern.exec(text)
+    }
+
+    // return the collection
+    return result.join("; ")
 }
