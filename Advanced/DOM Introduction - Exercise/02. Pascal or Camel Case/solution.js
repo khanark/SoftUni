40e1231b595965f8e6d45db2a5bf9fdc6e0d5d55
JScodeKiddie <JScodeKiddie@gmail.com>
2022-07-08 21:38:27 +0200

    updated Pascal or Camel Case Problem

--- Advanced/DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
+++ Advanced/DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
@@ -2,18 +2,33 @@ function solve() {
 	// get the elements
 	let string = document.getElementById('text').value;
 	const convention = document.getElementById('naming-convention').value;
-	const result = document.getElementById('result- container');
+	const result = document.getElementById('result');
 
+	// parse the string to lower case
 	string = string.toLowerCase()
-    console.log(string);
-	if (convention == 'Camel Case') {
-		string = string.split(" ")
-        for (let i = 0; i < string.length; i++) {
-            string[i].replace(string[i][0], string[i][0].toUpperCase())
-        }
-        console.log(string.join(""));
-	} else if (convention == 'Pascal Case') {
 
+	// change the casing according to the user input
+	if (convention == 'Camel Case') {
+		string = string
+			.split(' ')
+			.map((word, index) => {
+				if (index > 0) {
+					return word.replace(word[0], word[0].toUpperCase());
+				} else {
+					return word;
+				}
+			})
+			.join('');
+	} else if (convention == 'Pascal Case') {
+		string = string
+			.split(' ')
+			.map(word => word.replace(word[0], word[0].toUpperCase()))
+			.join('');
+		console.log(string);
+	} else {
+		string = 'Error!';
 	}
+
+	// render the new string
+	result.textContent = string;
 }
-//TODO: needs to be finished
