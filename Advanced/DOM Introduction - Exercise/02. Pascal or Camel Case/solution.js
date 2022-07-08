6720e3de6b8bf29229055d268d2c202671f25239
JScodeKiddie <JScodeKiddie@gmail.com>
2022-07-08 19:45:21 +0200

    formated Substract Problem

--- Advanced/DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
+++ Advanced/DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,18 @@
 function solve() {
-  //TODO...
+	// get the elements
+	let string = document.getElementById('text').value;
+	const convention = document.getElementById('naming-convention').value;
+	const result = document.getElementById('result- container');
+
+	string = string.toLowerCase()
+    console.log(string);
+	if (convention == 'Camel Case') {
+		string = string.split(" ")
+        for (let i = 0; i < string.length; i++) {
+            string[i].replace(string[i][0], string[i][0].toUpperCase())
+        }
+        console.log(string.join(""));
+	} else if (convention == 'Pascal Case') {
+
+	}
 }
