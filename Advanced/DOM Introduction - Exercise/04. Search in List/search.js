a8dc3453562642f696506e28b5f4af474d898d38
JScodeKiddie <JScodeKiddie@gmail.com>
2022-07-08 23:04:48 +0200

    updated Seach In List Problem

--- Advanced/DOM Introduction - Exercise/04. Search in List/search.js
+++ Advanced/DOM Introduction - Exercise/04. Search in List/search.js
@@ -1,3 +1,30 @@
 function search() {
-   // TODO
+	// get the elements
+	const input = document.getElementById('searchText').value;
+	const result = document.getElementById('result');
+	const towns = [...document.querySelectorAll('#towns li')];
+
+	// reset the towns every time user types different input
+	const resetTowns = () => {
+		towns.forEach(town => {
+			town.style.textDecoration = 'none';
+			town.style.fontWeight = 'normal';
+		});
+	};
+
+	resetTowns();
+
+	// filter all the towns by the user's input
+	const filteredTowns = towns.filter(town =>
+		town.textContent.includes(input)
+	);
+
+	// underline and bold the towns
+	filteredTowns.forEach(town => {
+		town.style.textDecoration = 'underline';
+		town.style.fontWeight = 'bold';
+	});
+
+	// render all the matched towns
+	result.textContent = `${filteredTowns.length} matches found`;
 }
