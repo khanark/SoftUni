ecc377e0650d30d772d58db809e4efbd2f284a16
JScodeKiddie <JScodeKiddie@gmail.com>
2022-07-08 18:42:31 +0200

    updated Substraction Problem

---
+++ Advanced/DOM Introduction - Exercise/01. Subtraction/subtract.js
@@ -0,0 +1,12 @@
+function subtract() {
+    // get the numbers
+    const firstNumber = document.getElementById("firstNumber").value
+    const secondNumber = document.getElementById("secondNumber").value
+
+    // do the math
+    const numberResult = Number(firstNumber) - Number(secondNumber)
+
+    // render the result
+    const result = document.getElementById("result")
+    result.textContent = numberResult
+}
