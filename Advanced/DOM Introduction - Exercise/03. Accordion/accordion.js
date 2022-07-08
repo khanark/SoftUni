20338c7eb0b5384ad6afaef591c688daa994df8e
JScodeKiddie <JScodeKiddie@gmail.com>
2022-07-08 22:15:59 +0200

    updated Accordion Problem

--- Advanced/DOM Introduction - Exercise/03. Accordion/accordion.js
+++ Advanced/DOM Introduction - Exercise/03. Accordion/accordion.js
@@ -1,3 +1,15 @@
 function toggle() {
-    console.log('TODO:...');
+    // get the elements
+	const btn = document.querySelector('.button');
+	const div = document.getElementById('extra');
+
+    // implement the logic
+	if (btn.textContent == 'More') {
+		div.style.display = 'block';
+	} else {
+		div.style.display = 'none';
+	}
+
+    // change the text content of the button
+	btn.textContent = btn.textContent == 'More' ? 'Less' : 'More';
 }
