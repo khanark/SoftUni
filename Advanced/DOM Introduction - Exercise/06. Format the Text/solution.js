d1d54d1fa8e564fdf14efc090f647d04d72f16c3
JScodeKiddie <JScodeKiddie@gmail.com>
2022-07-09 17:19:42 +0200

    updated Format the Text Problem

--- Advanced/DOM Introduction - Exercise/06. Format the Text/solution.js
+++ Advanced/DOM Introduction - Exercise/06. Format the Text/solution.js
@@ -1,3 +1,17 @@
 function solve() {
-  //TODO
+	// DOM elements
+	const input = document.getElementById('input').value;
+	const output = document.getElementById('output');
+
+	// split the user input by "." and filter out the empty strings
+	const sentences = input.split('.').filter(x => x != '');
+
+	// reset the innerHTML
+	output.innerHTML = '';
+
+	// loop and get every three sentences and group them into one paragraph
+	for (let i = 0; i < sentences.length; i += 3) {
+		const paragraph = sentences.slice(i, 3 + i).join('.');
+		output.innerHTML += '<p>' + paragraph + '.' + '</p>';
+	}
 }
