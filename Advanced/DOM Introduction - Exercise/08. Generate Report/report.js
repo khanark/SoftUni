f83eecfafe6fd684e32c7cc48c4a6957c29d8ad0
JScodeKiddie <JScodeKiddie@gmail.com>
2022-07-12 21:16:31 +0200

    updated Generate Report Problem {2}

--- Advanced/DOM Introduction - Exercise/08. Generate Report/report.js
+++ Advanced/DOM Introduction - Exercise/08. Generate Report/report.js
@@ -1,19 +1,30 @@
 function generateReport() {
+	// DOM elements
 	const headers = [...document.querySelectorAll('input[type=checkbox]')];
 	const length = [...document.querySelectorAll('tbody tr')].length;
 	const output = document.getElementById('output');
 	const result = [];
 
+	// sort the headers that are selected via checkboxes
 	const selectedHeaders = headers.filter(el => el.checked);
+
+	// loop table's length (rows)
 	for (let i = 0; i < length; i++) {
-		const obj = {};
+		// store current data
+		const data = {};
+
+		// for each selected header
 		selectedHeaders.forEach(header => {
 			const index = headers.indexOf(header);
 			const path = `tbody tr td:nth-child(${index + 1})`;
 			const value = [...document.querySelectorAll(path)][i].textContent;
-			obj[header.name] = value;
+			data[header.name] = value;
 		});
-		result.push(obj);
+
+		// push the data to the result array
+		result.push(data);
 	}
+
+	// render the output
 	output.textContent = JSON.stringify(result);
 }
