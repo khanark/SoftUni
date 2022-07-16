4c56f286a6e01298890fc5176c11dfa1b6462505
JScodeKiddie <JScodeKiddie@gmail.com>
2022-07-16 12:55:54 +0200

    updated Number Conventor Problem

--- Advanced/DOM Introduction - Exercise/09. Number Convertor/solution.js
+++ Advanced/DOM Introduction - Exercise/09. Number Convertor/solution.js
@@ -1,4 +1,39 @@
 function solve() {
+	// DOM elements
+	const input = document.getElementById('input');
+	const result = document.getElementById('result');
+	const menu = document.querySelector('#selectMenuTo');
+	const btn = document.getElementsByTagName('button')[0];
 
-    //TODO...
+	const optionToAppend = document.createElement('option');
+	menu.appendChild(optionToAppend);
+	const firstOptionTag = document.querySelector('#selectMenuTo > option');
+	const secondOptionTag = document.querySelector(
+		'#selectMenuTo option:nth-child(2)'
+	);
+
+	// creating first tag
+	firstOptionTag.setAttribute('value', 'binary');
+	firstOptionTag.textContent = 'Binary';
+
+	// creating second tag
+	secondOptionTag.setAttribute('selected', '');
+	secondOptionTag.setAttribute('value', 'hexadecimal');
+	secondOptionTag.textContent = 'Hexadecimal';
+
+	btn.addEventListener('click', () => {
+		const inputNumber = Number(input.value);
+		const options = Array.from(
+			document.querySelectorAll('#selectMenuTo option')
+		);
+		const selectedOption = options.filter(el => el.selected)[0].value;
+		switch (selectedOption) {
+			case 'hexadecimal':
+				result.value = inputNumber.toString(16).toUpperCase();
+				break;
+			case 'binary':
+				result.value = inputNumber.toString(2);
+				break;
+		}
+	});
 }
