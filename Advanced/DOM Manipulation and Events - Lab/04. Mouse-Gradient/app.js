371435b360e2fd441892984591f63774b1b7fed4
JScodeKiddie <JScodeKiddie@gmail.com>
2022-07-17 18:08:46 +0200

    updated Mouse-Gradient Problem

--- Advanced/DOM Manipulation and Events - Lab/04. Mouse-Gradient/app.js
+++ Advanced/DOM Manipulation and Events - Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,13 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+	// DOM elements
+	const div = document.getElementById('gradient');
+
+	div.addEventListener('mousemove', onMove);
+
+	function onMove(ev) {
+		const percent = Math.floor(
+			(Number(ev.offsetX) / Number(ev.target.clientWidth)) * 100
+		);
+		document.getElementById('result').textContent = `${percent}%`;
+	}
 }
