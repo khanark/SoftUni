eb8553d51a5e39d4856fbb424a57d5ed52cc7cd2
JScodeKiddie <JScodeKiddie@gmail.com>
2022-07-17 17:09:03 +0200

    updated Delete-From-Table Problem

--- Advanced/DOM Manipulation and Events - Lab/02. Delete-From-Table/app.js
+++ Advanced/DOM Manipulation and Events - Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,18 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+	// DOM element
+	const input = document.querySelector('input[name="email"]');
+	const matches = Array.from(
+		document.querySelectorAll('tbody td:last-child')
+	);
+	const output = document.querySelector('#result');
+
+	const result = matches.filter(td => td.textContent == input.value);
+
+	// output
+	if (result.length > 0) {
+		result.forEach(td => td.parentNode.remove());
+		output.textContent = 'Deleted.';
+	} else {
+		output.textContent = 'Not found.';
+	}
 }
