93832fa1daa40098b8b62c21b6642e0df65131c2
JScodeKiddie <JScodeKiddie@gmail.com>
2022-07-17 19:49:52 +0200

    updated Dynamic-Validation Problem

--- Advanced/DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
+++ Advanced/DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,14 @@
 function validate() {
-    console.log('TODO:...');
+	const pattern = /[a-z]+@[a-z]+.[a-z]+/;
+	const input = document.getElementById('email');
+
+	input.addEventListener('change', onChange);
+
+	function onChange(e) {
+		if (pattern.test(input.value)) {
+			input.classList.remove('error');
+		} else {
+			input.classList.add('error');
+		}
+	}
 }
