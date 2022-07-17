be6c3fa32d4864c3122e1c46c6e51668c76c205a
JScodeKiddie <JScodeKiddie@gmail.com>
2022-07-17 17:43:08 +0200

    updated  Add-Delete Problem

--- Advanced/DOM Manipulation and Events - Lab/03. Add-Delete/app.js
+++ Advanced/DOM Manipulation and Events - Lab/03. Add-Delete/app.js
@@ -1,3 +1,26 @@
 function addItem() {
-    //TODO...
+	const items = document.querySelector('#items');
+	const input = document.getElementById('newItemText');
+	const newLi = createElement('li', input.value);
+	const newA = createElement('a', '[Delete]');
+	newA.href = '#';
+
+	items.appendChild(newLi);
+	newLi.appendChild(newA);
+
+	items.addEventListener('click', onClick);
+
+	function onClick(ev) {
+		if ((ev.target.tagName = 'a')) {
+			const liItem = ev.target.parentNode;
+			items.removeChild(liItem);
+			input.value = '';
+		}
+	}
+
+	function createElement(element, content) {
+		const newElement = document.createElement(element);
+		newElement.textContent = content;
+		return newElement;
+	}
 }
