948cb4f8244e96c7d08572ebe5654f80b454a6c8
JScodeKiddie <JScodeKiddie@gmail.com>
2022-07-17 19:43:14 +0200

    updated Highlight-Active Problem

--- Advanced/DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
+++ Advanced/DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
@@ -1,3 +1,16 @@
 function focused() {
-    console.log('TODO:...');
+	const inputFields = Array.from(document.querySelectorAll('input'));
+
+	inputFields.forEach(el => {
+		el.addEventListener('focus', onFocus);
+		el.addEventListener('blur', onBlur);
+	});
+
+	function onFocus(e) {
+		e.target.parentNode.classList.add('focused');
+	}
+
+	function onBlur(e) {
+		e.target.parentNode.classList.remove('focused');
+	}
 }
