5b55bd4354ca951605dec9b2f17279d9296e3cf6
JScodeKiddie <JScodeKiddie@gmail.com>
2022-07-17 20:54:39 +0200

    updated Shopping-Cart Problem

--- Advanced/DOM Manipulation and Events - Lab/07. Shopping-Cart/solution.js
+++ Advanced/DOM Manipulation and Events - Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,38 @@
+/* eslint-disable no-undef */
 function solve() {
-   //TODO...
+	const div = document.querySelector('.shopping-cart');
+	const output = document.querySelector('textarea');
+	const checkOut = document.querySelector('.checkout');
+	const items = { products: new Set(), totalPrice: 0 };
+
+	div.addEventListener('click', onClick);
+
+	function onClick(ev) {
+		if (
+			ev.target.tagName == 'BUTTON' &&
+			ev.target.className == 'add-product'
+		) {
+			const productInfo = ev.target.parentNode.parentNode;
+			const price = productInfo.querySelector(
+				'.product-line-price'
+			).textContent;
+			const productName = productInfo.querySelector(
+				'div > .product-title '
+			).textContent;
+			output.textContent += `Added ${productName} for ${price} to the cart.\n`;
+
+			items.products.add(productName);
+			items.totalPrice += Number(price);
+		}
+	}
+	checkOut.addEventListener(
+		'click',
+		(f = e => {
+			output.textContent += `You bought ${Array.from(items.products).join(
+				', '
+			)} for ${items.totalPrice.toFixed(2)}.`;
+			div.removeEventListener('click', onClick);
+			checkOut.removeEventListener('click', f);
+		})
+	);
 }
