1566803a2675b9dbf8c15cf1a44c886717d7121f
JScodeKiddie <JScodeKiddie@gmail.com>
2022-07-17 16:47:20 +0200

    update problem List-Of-Items

--- Advanced/DOM Manipulation and Events - Lab/01. List-Of-Items/app.js
+++ Advanced/DOM Manipulation and Events - Lab/01. List-Of-Items/app.js
@@ -1,3 +1,6 @@
 function addItem() {
-    console.log('TODO:...');
+	// DOM elements
+	const newLi = document.createElement('li');
+	newLi.textContent = document.querySelector('#newItemText').value;
+	document.getElementById('items').appendChild(newLi);
 }
