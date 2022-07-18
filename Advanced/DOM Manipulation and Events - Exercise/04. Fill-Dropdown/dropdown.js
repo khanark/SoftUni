db964d1b063380a88e95a6eec10f1864c1177c77
JScodeKiddie <JScodeKiddie@gmail.com>
2022-07-18 20:24:07 +0200

    updated Fill-Dropdown Problem

--- Advanced/DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
+++ Advanced/DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,22 @@
 function addItem() {
-    console.log('TODO:...');
+	// DOM elements
+	const itemText = document.querySelector('#newItemText');
+	const itemValue = document.querySelector('#newItemValue');
+
+	// create new DOM element
+	const newOption = createElement('option', itemText.value, itemValue.value);
+
+	// append the newly created element to the select tag
+	document.querySelector('#menu').appendChild(newOption);
+
+	// reset the input fields
+	itemText.value = '';
+	itemValue.value = '';
+
+	function createElement(type, content, value) {
+		const element = document.createElement(type);
+		element.textContent = content;
+		element.value = value;
+		return element;
+	}
 }
