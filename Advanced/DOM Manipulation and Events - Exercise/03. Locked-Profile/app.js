121a82cc1272aa8786c434969c85a76ed57bda59
JScodeKiddie <JScodeKiddie@gmail.com>
2022-07-18 20:08:22 +0200

    updated Locked-Profile Problem

--- Advanced/DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
+++ Advanced/DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,27 @@
 function lockedProfile() {
-    console.log('TODO...')
+	// DOM elements
+	const btns = Array.from(document.querySelectorAll('button'));
+
+	// adding event to each button
+	btns.forEach(btn => {
+		btn.addEventListener('click', showMore);
+	});
+
+	// creating showMore function to show the user hidden fields
+	function showMore(e) {
+		const currentDiv = e.target.parentNode;
+		const stage = Array.from(
+			currentDiv.querySelectorAll('[type="radio"]')
+		).filter(el => el.checked)[0].value;
+
+		if (currentDiv.children[9].style.display == 'inline') {
+			if (stage == 'unlock') {
+				currentDiv.children[9].style.display = 'none';
+				e.target.textContent = 'Show more';
+			}
+		} else if (stage == 'unlock') {
+			currentDiv.children[9].style.display = 'inline';
+			e.target.textContent = 'Hide it';
+		}
+	}
 }
