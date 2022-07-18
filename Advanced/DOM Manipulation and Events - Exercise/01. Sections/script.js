aae4a195cff3919585c9fd54d1b0a303a05265b4
JScodeKiddie <JScodeKiddie@gmail.com>
2022-07-18 18:31:52 +0200

    updated Sections Problem

--- Advanced/DOM Manipulation and Events - Exercise/01. Sections/script.js
+++ Advanced/DOM Manipulation and Events - Exercise/01. Sections/script.js
@@ -1,3 +1,21 @@
 function create(words) {
-   console.log('TODO:...');
+	const div = document.querySelector('#content');
+	const divs = words.map(text => createElement('div', 'p', text));
+
+	divs.forEach(currentDiv => div.appendChild(currentDiv));
+
+	divs.forEach(div => div.addEventListener('click', display));
+
+	function display(e) {
+		e.target.querySelector('p').style.display = '';
+	}
+
+	function createElement(type, child, content) {
+		const element = document.createElement(type);
+		const childToAppend = document.createElement(child);
+		childToAppend.textContent = content;
+		childToAppend.style.display = 'none';
+		element.appendChild(childToAppend);
+		return element;
+	}
 }
--- Advanced/DOM Manipulation and Events - Exercise/02. Time-Converter/solution.js
+++ Advanced/DOM Manipulation and Events - Exercise/02. Time-Converter/solution.js
@@ -1,4 +1,35 @@
 function attachEventsListeners() {
+	const btns = Array.from(document.querySelectorAll('[type="button"]'));
 
-    console.log('TODO:...');
+	btns.forEach(btn => btn.addEventListener('click', timeConvertor));
+
+	function timeConvertor(e) {
+		const timeType = e.target.parentNode.children[1].id;
+		const value = Number(e.target.parentNode.children[1].value);
+		let seconds = 0;
+
+		switch (timeType) {
+			case 'days':
+				seconds = value * 24 * 60 * 60;
+				break;
+			case 'hours':
+				seconds = value * 60 * 60;
+				break;
+			case 'minutes':
+				seconds = value * 60;
+				break;
+			case 'seconds':
+				seconds = value;
+				break;
+		}
+
+		const minutes = seconds / 60;
+		const hours = minutes / 60;
+		const days = hours / 24;
+
+		document.querySelector('#days').value = days;
+		document.querySelector('#hours').value = hours;
+		document.querySelector('#minutes').value = minutes;
+		document.querySelector('#seconds').value = seconds;
+	}
 }
