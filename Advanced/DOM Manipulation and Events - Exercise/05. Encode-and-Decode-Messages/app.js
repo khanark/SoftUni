c6ebccc6795a43ef0e75fb32f11bb641a3a6ec73
JScodeKiddie <JScodeKiddie@gmail.com>
2022-07-18 20:51:48 +0200

    updated Encode-and-Decode-Messages Problem

--- Advanced/DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
+++ Advanced/DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,32 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+	// DOM Buttons
+	const encodeBtn = document.querySelector('#main > div button');
+	const decodeBtn = document.querySelector('#main div:nth-child(2) button');
+
+	// empty string to store the decoded text
+	let decodedString = '';
+
+	encodeBtn.addEventListener('click', encode);
+	decodeBtn.addEventListener('click', decode);
+
+	// encoding function
+	function encode(e) {
+		const currentDiv = e.target.parentNode;
+		const text = currentDiv.children[1];
+		decodedString = text.value;
+
+		const result = text.value
+			.split('')
+			.map(letter => String.fromCharCode(Number(letter.charCodeAt() + 1)))
+			.join('');
+
+		currentDiv.parentNode.children[1].children[1].value = result;
+		text.value = '';
+	}
+
+	// decoding function
+	function decode(e) {
+		const currentDiv = e.target.parentNode;
+		currentDiv.parentNode.children[1].children[1].value = decodedString;
+	}
 }
