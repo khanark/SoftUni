2e98243f31a16a5df19c38b6d75d2329f06239a3
JScodeKiddie <JScodeKiddie@gmail.com>
2022-07-23 09:44:53 +0200

    fixed solution.js for the "Furniture" problem

--- Advanced/DOM Manipulation and Events - Exercise/06. Furniture/solution.js
+++ Advanced/DOM Manipulation and Events - Exercise/06. Furniture/solution.js
@@ -1,4 +1,48 @@
 function solve() {
+    const input = document.querySelector('textarea');
+    const output = document.querySelector('textarea:last-of-type');
+    const generateBtn = document.querySelector('button');
+    const buyBtn = document.querySelector('button:last-of-type');
+    const tbody = document.querySelector('tbody');
 
-  //TODO...
+    generateBtn.addEventListener('click', onGenerate);
+    buyBtn.addEventListener('click', onBuy);
+
+    function onGenerate() {
+        const data = JSON.parse(input.value);
+        data.forEach(
+            (obj) =>
+                (tbody.innerHTML += `<tr>
+         <td><img src=${obj.img}></td>
+         <td><p>${obj.name}</p></td>
+         <td><p>${obj.price}</p></td>
+         <td><p>${obj.decFactor}</p></td>
+         <td><input type="checkbox"/></td>
+     </tr>`)
+        );
+    }
+
+    function onBuy() {
+        const checkBoxes = [...document.querySelectorAll('input:checked')];
+        const productNames = [];
+        let totalPrice = 0;
+        let decFactor = 0;
+
+        checkBoxes.forEach((box) => {
+            const parent = box.parentNode.parentNode;
+            const productName = parent.children[1].textContent;
+            const price = Number(parent.children[2].textContent);
+            const factor = Number(parent.children[3].textContent);
+
+            productNames.push(productName);
+            totalPrice += price;
+            decFactor += factor;
+        });
+
+        output.textContent += `Bought furniture: ${productNames.join(', ')}\n`;
+        output.textContent += `Total price: ${totalPrice.toFixed(2)}\n`;
+        output.textContent += `Average decoration factor: ${
+            decFactor / productNames.length
+        }`;
+    }
 }
