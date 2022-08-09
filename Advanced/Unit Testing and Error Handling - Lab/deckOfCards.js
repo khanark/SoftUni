05827fdf15564e9e93e58696cc7fea24f00ac838
JScodeKiddie <JScodeKiddie@gmail.com>
2022-08-09 18:09:29 +0200

    added deckOfCards.js

--- Advanced/Unit Testing and Error Handling - Lab/deckOfCards.js
+++ Advanced/Unit Testing and Error Handling - Lab/deckOfCards.js
@@ -1,5 +1,7 @@
 function printDeckOfCards(cards) {
-    function createCard(face, suit) {
+    // create function to create cards
+    function createCard() {
+        // store the valid faces
         const validFaces = [
             '2',
             '3',
@@ -15,6 +17,8 @@ function printDeckOfCards(cards) {
             'K',
             'A',
         ];
+
+        // store the valid suits
         const validSuits = {
             S: '\u2660',
             H: '\u2665',
@@ -22,18 +26,29 @@ function printDeckOfCards(cards) {
             C: '\u2663',
         };
 
-        if (!validFaces.includes(face)) {
-            throw new Error();
-        }
-        if (!validSuits.hasOwnProperty(suit)) {
-            throw new Error();
-        }
-        return {
-            face,
-            suit,
-            toString: () => `${face}${validSuits[suit]}`,
-        };
+        // return cards as array if valid // else throw an error
+        return cards
+            .map(card => {
+                const cardLength = card.length - 1;
+                const checkIfIs10 = () =>
+                    card.includes('10') ? '10' : card[0];
+                if (
+                    !validFaces.includes(checkIfIs10()) ||
+                    !validSuits[card[cardLength]]
+                ) {
+                    throw new Error(`Invalid card: ${card}`);
+                } else {
+                    return `${checkIfIs10()}${validSuits[card[cardLength]]}`;
+                }
+            })
+            .join(' ');
     }
+
+    // try if an error is thrown and if so catch and output the error message
     try {
-    } catch (er) {}
+        console.log(createCard());
+    } catch (err) {
+        console.log(err.message.toString());
+    }
 }
+printDeckOfCards(['AS', '10D', 'KH', '2C']);
