971c24df30be4a9a834d19695ce335ad2df4b6aa
JScodeKiddie <JScodeKiddie@gmail.com>
2022-08-09 05:23:29 +0200

    added deckOfCards.js

---
+++ Advanced/Unit Testing and Error Handling - Lab/deckOfCards.js
@@ -0,0 +1,39 @@
+function printDeckOfCards(cards) {
+    function createCard(face, suit) {
+        const validFaces = [
+            '2',
+            '3',
+            '4',
+            '5',
+            '6',
+            '7',
+            '8',
+            '9',
+            '10',
+            'J',
+            'Q',
+            'K',
+            'A',
+        ];
+        const validSuits = {
+            S: '\u2660',
+            H: '\u2665',
+            D: '\u2666',
+            C: '\u2663',
+        };
+
+        if (!validFaces.includes(face)) {
+            throw new Error();
+        }
+        if (!validSuits.hasOwnProperty(suit)) {
+            throw new Error();
+        }
+        return {
+            face,
+            suit,
+            toString: () => `${face}${validSuits[suit]}`,
+        };
+    }
+    try {
+    } catch (er) {}
+}
