529518068d045642f8007bdd0886d31e4fe87710
JScodeKiddie <JScodeKiddie@gmail.com>
2022-11-09 22:53:02 +0100

    uploaded: 03.Search-in-List

--- Applications/Client Side Rendering - Exercise/03.Search-in-List/search.js
+++ Applications/Client Side Rendering - Exercise/03.Search-in-List/search.js
@@ -1,3 +1,29 @@
-function search() {
-   // TODO
+import { render, html } from '/lib.js';
+import templateTown from '/templates/templateTown.js';
+import { towns } from './towns.js';
+
+const section = document.getElementById('towns');
+const result = document.getElementById('result');
+const input = document.getElementById('searchText');
+
+window.addEventListener('DOMContentLoaded', loadTowns);
+
+function loadTowns() {
+  render(
+    html`
+      <ul>
+        ${towns.map(templateTown)}
+      </ul>
+    `,
+    section
+  );
 }
+
+document.querySelector('button').addEventListener('click', () => {
+  const matches = Array.from(section.querySelectorAll('ul li')).filter(el => {
+    return el.textContent.includes(input.value);
+  });
+
+  matches.forEach(el => el.classList.toggle('active'));
+  render(html`${matches.length} matches found`, result);
+});
---
+++ Applications/Client Side Rendering - Exercise/03.Search-in-List/templates/templateTown.js
@@ -0,0 +1,5 @@
+import { html } from '/lib.js';
+
+const townTamplate = town => html`<li>${town}</li>`;
+
+export default townTamplate;
