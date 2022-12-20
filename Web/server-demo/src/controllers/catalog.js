10069dcd00501759cfa4af6b403c7a2c6b1708c6
JScodeKiddie <JScodeKiddie@gmail.com>
2022-12-20 22:45:25 +0100

    added some styling for better readibility

--- Web/server-demo/src/controllers/catalog.js
+++ Web/server-demo/src/controllers/catalog.js
@@ -1,74 +1,77 @@
 const { loadFragment, render } = require('../view');
 const {
-    getProducts,
-    addProduct,
-    getSingleProduct,
-    editProduct,
+  getProducts,
+  addProduct,
+  getSingleProduct,
+  editProduct,
 } = require('../data');
 const querystring = require('node:querystring');
 
 module.exports = {
-    async catalog(req, res) {
-        const products = await getProducts();
-        loadFragment('catalog', fragment => {
-            const newFragment = fragment
-                .toString()
-                .replace('{{items}}', () =>
-                    products
-                        .map(
-                            p =>
-                                `<li class="list">${p.name} - ${p.price}$ <a href="/edit?id=${p._id}">[edit]</a></li>`
-                        )
-                        .join('')
-                );
-            res.html(render(newFragment, 'Catalog'));
-        });
-    },
-    createGet(req, res) {
-        loadFragment('create', fragment => {
-            res.html(render(fragment, 'Create Product'));
-        });
-    },
-    createPost(req, res) {
-        let buffer = '';
-        req.on('data', chunk => {
-            buffer += chunk.toString();
-        });
-        req.on('end', async () => {
-            const result = querystring.decode(buffer);
-            await addProduct(result.name, +result.price);
-            res.writeHead(301, {
-                Location: '/catalog',
-            });
-            res.end();
-        });
-    },
-    async editGet(req, res) {
-        const productId = req.url.searchParams.get('id');
+  async catalog(req, res) {
+    const products = await getProducts();
+    loadFragment('catalog', fragment => {
+      const newFragment = fragment
+        .toString()
+        .replace('{{items}}', () =>
+          products
+            .map(
+              p =>
+                `<li class="list">${p.name} - ${p.price}$ <a href="/edit?id=${p._id}">[edit]</a></li>`
+            )
+            .join('')
+        );
+      res.html(render(newFragment, 'Catalog'));
+    });
+  },
+  
+  createGet(req, res) {
+    loadFragment('create', fragment => {
+      res.html(render(fragment, 'Create Product'));
+    });
+  },
 
-        const product = await getSingleProduct(productId);
-        loadFragment('edit', fragment => {
-            const newFragment = fragment
-                .toString()
-                .replace('{{_id}}', productId)
-                .replace('{{name}}', product.name)
-                .replace('{{price}}', product.price);
-            res.html(render(newFragment));
-        });
-    },
-    async editPost(req, res) {
-        let buffer = '';
-        req.on('data', chunk => {
-            buffer += chunk.toString();
-        });
-        req.on('end', async () => {
-            const productId = req.url.searchParams.get('id');
-            const data = querystring.decode(buffer);
-            await editProduct(productId, data);
-            res.writeHead(301, {
-                Location: '/catalog',
-            });
-            res.end();
-        });
-    },
+  createPost(req, res) {
+    let buffer = '';
+    req.on('data', chunk => {
+      buffer += chunk.toString();
+    });
+    req.on('end', async () => {
+      const result = querystring.decode(buffer);
+      await addProduct(result.name, +result.price);
+      res.writeHead(301, {
+        Location: '/catalog',
+      });
+      res.end();
+    });
+  },
+
+  async editGet(req, res) {
+    const productId = req.url.searchParams.get('id');
+    const product = await getSingleProduct(productId);
+    loadFragment('edit', fragment => {
+      const newFragment = fragment
+        .toString()
+        .replace('{{_id}}', productId)
+        .replace('{{name}}', product.name)
+        .replace('{{price}}', product.price);
+      res.html(render(newFragment));
+    });
+  },
+
+  async editPost(req, res) {
+    let buffer = '';
+    req.on('data', chunk => {
+      buffer += chunk.toString();
+    });
+    req.on('end', async () => {
+      const productId = req.url.searchParams.get('id');
+      const data = querystring.decode(buffer);
+      await editProduct(productId, data);
+      res.writeHead(301, {
+        Location: '/catalog',
+      });
+      res.end();
+    });
+  },
 };
--- Web/server-demo/src/router.js
+++ Web/server-demo/src/router.js
@@ -1,48 +1,45 @@
 const routes = {};
 
 const register = (method, path, handler) => {
-    if (!routes[path]) {
-        routes[path] = {};
-    }
-    routes[path][method] = handler;
+  if (!routes[path]) {
+    routes[path] = {};
+  }
+  routes[path][method] = handler;
 };
 
 const getItem = register.bind(null, 'GET');
 const postItem = register.bind(null, 'POST');
 
 const match = (req, res) => {
-    const url = new URL(req.url, `http://${req.headers.host}`);
-    req.url = url
-
-    let handler;
-    const actions = routes[url.pathname];
-
-    if (actions) {
-        handler = actions[req.method];
-    }
-
-    if (typeof handler == 'function') {
-        res.html = page => {
-            res.writeHead(200, {
-                'Content-Type': 'text/html; utf-8',
-            });
-            res.write(page);
-            res.end();
-        };
-        handler(req, res);
-    } else {
-        defaultController(req, res);
-    }
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  req.url = url;
+  let handler;
+  const actions = routes[url.pathname];
+  if (actions) {
+    handler = actions[req.method];
+  }
+  if (typeof handler == 'function') {
+    res.html = page => {
+      res.writeHead(200, {
+        'Content-Type': 'text/html; utf-8',
+      });
+      res.write(page);
+      res.end();
+    };
+    handler(req, res);
+  } else {
+    defaultController(req, res);
+  }
 };
 
 function defaultController(req, res) {
-    res.writeHead(404);
-    res.write('Not Found');
-    res.end();
+  res.writeHead(404);
+  res.write('Not Found');
+  res.end();
 }
 
 module.exports = {
-    getItem,
-    postItem,
-    match,
+  getItem,
+  postItem,
+  match,
 };
