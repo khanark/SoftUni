53b332f28b9842d05af6c3bf7ccb7a2a09457d14
JScodeKiddie <JScodeKiddie@gmail.com>
2023-01-05 18:26:15 +0100

    added router from expressJS

--- Web/cubicle/controllers/home.js
+++ Web/cubicle/controllers/home.js
@@ -1,4 +1,9 @@
-module.exports = async (req, res) => {
-  const cubes = await req.storage.getCubes();
+const express = require("express");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const cubes = await req.storage.getCubes(req.query);
   res.render('index', { cubes });
-};
+})
+
+module.exports = router
