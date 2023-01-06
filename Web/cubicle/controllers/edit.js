0fb6266f1ab47e7d7752aa03963ec2d3debc3942
JScodeKiddie <JScodeKiddie@gmail.com>
2023-01-06 11:24:04 +0100

    added edit controller

---
+++ Web/cubicle/controllers/edit.js
@@ -0,0 +1,10 @@
+module.exports = async (req, res) => {
+  const data = {
+    name: req.body.name.toLocaleUpperCase(),
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    difficulty: Number(req.body.difficultyLevel),
+  };
+  await req.storage.editCube(req.params.id, data);
+  res.redirect(`/details/${req.params.id}`);
+};
