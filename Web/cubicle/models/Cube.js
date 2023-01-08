4dd40bd5901f3ece01a79ce387be924944232fee
JScodeKiddie <JScodeKiddie@gmail.com>
2023-01-08 10:19:11 +0100

    created Cube schema

---
+++ Web/cubicle/models/Cube.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const connection = 'mongodb://127.0.0.1:27017/cubes';
+
+const cubeSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  difficulty: { 
+    type: Number,
+    min: [0, "The minimum required value is 0"],
+    max: [6, "The value cannot exceed 6"]
+   },
+});
+
+start().catch(err => console.log(err));
+
+async function start() {
+  await mongoose.connect(connection);
+}
+
+const Cube = model('Cube', cubeSchema);
+
+module.exports = Cube
