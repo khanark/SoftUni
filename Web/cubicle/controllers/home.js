811f5b7ffec80387288932c3adb1985d46708e3b
JScodeKiddie <JScodeKiddie@gmail.com>
2023-01-09 19:47:59 +0100

    passed cubes without refactoring

--- Web/cubicle/controllers/home.js
+++ Web/cubicle/controllers/home.js
@@ -1,11 +1,8 @@
 const express = require('express');
-const { matchDiff } = require('../utils/utils');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
   const cubes = await req.storage.getCubes(req.query);
-  cubes.forEach(c => (c.difficulty = matchDiff(c.difficulty)));
-
   res.render('index', { cubes });
 });
 
