60b919c85f882ec8bfabf68e446f6acb8bcb35db
JScodeKiddie <JScodeKiddie@gmail.com>
2023-01-12 21:11:03 +0100

    added attach page for single cube after pressing on the attach button from the details page.

--- Web/cubicle/controllers/details.js
+++ Web/cubicle/controllers/details.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { matchSelected } = require('../utils/utils');
+const { dataViewModel } = require('../utils/utils');
 const router = express.Router();
 
 router.get('/delete/:id', async (req, res) => {
@@ -16,6 +16,13 @@ router.get('/edit/:id', async (req, res) => {
   const cube = await req.storage.getSingleCube(req.params.id);
   cube.options = matchSelected(cube.difficulty);
   res.render('edit', { cube });
+});
+
+router.get('/attach/:id', async (req, res) => {
+  const cube = dataViewModel(await req.storage.getSingleCube(req.params.id));
+  // console.log(dataViewModel(cube));
+  // [] get the corresponding cube and pass it to the template
+  res.render('attachAccessory', { cube });
 });
 
 module.exports = router;
