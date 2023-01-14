570aae9024671d24db683cc0ff6a5b6ade7104bb
JScodeKiddie <JScodeKiddie@gmail.com>
2023-01-14 14:58:34 +0100

    added service for accessories

---
+++ Web/cubicle/services/accessories.js
@@ -0,0 +1,19 @@
+const Accessory = require("../models/Accessory")
+const { accessoryViewModel } = require("../utils/utils")
+
+module.exports = (req, res, next) => {
+  req.accessory = {
+    addAccessory,
+    getAccessories
+  }
+  next()
+}
+
+async function addAccessory(accessory) {
+  await Accessory.create(accessory)
+}
+
+async function getAccessories() {
+  const data = await Accessory.find({})
+  return data.map(accessoryViewModel)
+}
