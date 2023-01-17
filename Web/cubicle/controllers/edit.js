64d3f97c5deaad770b966351c396f830ac75c113
JScodeKiddie <JScodeKiddie@gmail.com>
2023-01-17 15:30:55 +0100

    updated edit controller

--- Web/cubicle/controllers/edit.js
+++ Web/cubicle/controllers/edit.js
@@ -1,4 +1,14 @@
-module.exports = async (req, res) => {
+const express = require('express');
+const router = express.Router();
+const { matchSelected } = require('../utils/utils');
+
+router.get('/:id', async (req, res) => {
+  const cube = await req.storage.getSingleCube(req.params.id);
+  cube.options = matchSelected(cube.difficulty);
+  res.render('edit', { cube });
+});
+
+router.post('/:id', async (req, res) => {
   const data = {
     name: req.body.name.toLocaleUpperCase(),
     description: req.body.description,
@@ -7,4 +17,6 @@ module.exports = async (req, res) => {
   };
   await req.storage.updateCube(req.params.id, data);
   res.redirect(`/details/${req.params.id}`);
-};
+});
+
+module.exports = router;
