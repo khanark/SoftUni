c95c9d772b203b5dc2f1f6c016abadc2296b8bb1
JScodeKiddie <JScodeKiddie@gmail.com>
2023-01-17 15:31:07 +0100

    added login controller

---
+++ Web/cubicle/controllers/login.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.render('login');
+});
+
+router.post('/', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    await req.auth.login(username, password);
+    res.redirect('/');
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = router;
