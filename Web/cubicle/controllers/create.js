36275d76ee061c759618c7580cd8a4f6e06c64ee
JScodeKiddie <JScodeKiddie@gmail.com>
2023-01-17 15:30:27 +0100

    updated create controller

--- Web/cubicle/controllers/create.js
+++ Web/cubicle/controllers/create.js
@@ -9,13 +9,7 @@ router.post('/', async (req, res) => {
   if (Object.values(req.body).some(v => v == '')) {
     res.sendStatus(400);
   } else {
-    const data = {
-      name: req.body.name.toLocaleUpperCase(),
-      description: req.body.description,
-      imageUrl: req.body.imageUrl,
-      difficulty: Number(req.body.difficultyLevel),
-    };
-    await req.storage.createCube(data);
+    await req.storage.createCube(req.body, req.session);
     res.redirect('/');
   }
 });
