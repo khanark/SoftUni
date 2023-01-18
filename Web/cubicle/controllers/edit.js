e2491da3a4fde4cfb838d8086d3fe392149b5ffd
JScodeKiddie <JScodeKiddie@gmail.com>
2023-01-18 12:24:18 +0100

    updated edit controller

--- Web/cubicle/controllers/edit.js
+++ Web/cubicle/controllers/edit.js
@@ -4,19 +4,27 @@ const { matchSelected } = require('../utils/utils');
 
 router.get('/:id', async (req, res) => {
   const cube = await req.storage.getSingleCube(req.params.id);
-  cube.options = matchSelected(cube.difficulty);
-  res.render('edit', { cube });
+  if (cube.ownerId != req.session.user.id) {
+    res.redirect('/login');
+  } else {
+    cube.options = matchSelected(cube.difficulty);
+    res.render('edit', { cube });
+  }
 });
 
 router.post('/:id', async (req, res) => {
-  const data = {
-    name: req.body.name.toLocaleUpperCase(),
-    description: req.body.description,
-    imageUrl: req.body.imageUrl,
-    difficulty: Number(req.body.difficultyLevel),
-  };
-  await req.storage.updateCube(req.params.id, data);
-  res.redirect(`/details/${req.params.id}`);
+  if (
+    !(await req.storage.updateCube(
+      req.params.id,
+      req.body,
+      req.session.user.id
+    ))
+  ) {
+    res.redirect('/login');
+  } else {
+    await req.storage.updateCube(req.params.id, req.body, req.session.user.id);
+    res.redirect(`/details/${req.params.id}`);
+  }
 });
 
 module.exports = router;
