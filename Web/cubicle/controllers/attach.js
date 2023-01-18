faba62de78d227e7dc81110b872bce9ae8dec863
JScodeKiddie <JScodeKiddie@gmail.com>
2023-01-18 12:24:10 +0100

    updated delete controller

--- Web/cubicle/controllers/attach.js
+++ Web/cubicle/controllers/attach.js
@@ -4,17 +4,25 @@ const router = express.Router();
 router.post('/:id', async (req, res) => {
   const id = req.params.id;
   const accessoryId = req.body.accessory;
-  await req.storage.attachAccessory(id, accessoryId);
-  res.redirect(`/`);
+  if (
+    !(await req.storage.attachAccessory(id, accessoryId, req.session.user.id))
+  ) {
+    res.redirect('/login');
+  } else {
+    await req.storage.attachAccessory(id, accessoryId, req.session.user.id);
+    res.redirect(`/`);
+  }
 });
 
 router.get('/:id', async (req, res) => {
-  console.log(req.params.id);
-
   const [cube, accessories] = await Promise.all([
     req.storage.getSingleCube(req.params.id),
     req.accessory.getAccessories(),
   ]);
+
+  if (cube.ownerId != req.session.user.id) {
+    return res.redirect('/login');
+  }
 
   const cubeAccessories = cube.accessories.map(o => o.toString());
   const availableAccessories = accessories.filter(
--- Web/cubicle/controllers/delete.js
+++ Web/cubicle/controllers/delete.js
@@ -3,12 +3,20 @@ const router = express.Router();
 
 router.get('/:id', async (req, res) => {
   const cube = await req.storage.getSingleCube(req.params.id);
-  res.render('delete', { cube });
+  if (cube.ownerId != req.session.user.id) {
+    res.redirect('/login');
+  } else {
+    res.render('delete', { cube });
+  }
 });
 
 router.post('/:id', async (req, res) => {
-  await req.storage.deleteCube(req.params.id);
-  res.redirect('/');
+  if (!(await req.storage.deleteCube(req.params.id, req.session.user.id))) {
+    res.redirect('/login');
+  } else {
+    await req.storage.deleteCube(req.params.id, req.session.user.id);
+    res.redirect('/');
+  }
 });
 
 module.exports = router;
