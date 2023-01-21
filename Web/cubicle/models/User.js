66095499f6757fd46336e3920da39f57e25adf76
JScodeKiddie <JScodeKiddie@gmail.com>
2023-01-21 21:26:29 +0100

    added validation in the user schema

--- Web/cubicle/models/User.js
+++ Web/cubicle/models/User.js
@@ -1,9 +1,40 @@
 const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema({
-  username: { type: String, required: true, minLenght: 3 },
-  hashedPassword: { type: String, required: true },
+  username: {
+    type: String,
+    minLength: [5, 'Username should be atleast 5 characters long'],
+    validate: {
+      validator: function (val) {
+        return /[A-Za-z0-9]+/.test(val);
+      },
+      message: 'Password should contain only letters and numbers',
+    },
+  },
+  hashedPassword: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (val) {
+        return /[A-Za-z0-9]+/.test(val);
+      },
+      message: 'Password should contain only letters and numbers',
+    },
+    minLength: [8, 'Password should be atleast 8 characters long'],
+  },
 });
+
+// setting an index for the username (to make it unique)
+userSchema.index(
+  { username: 1 },
+  {
+    unique: true,
+    collation: {
+      locale: 'en',
+      strength: 2,
+    },
+  }
+);
 
 const User = model('User', userSchema);
 
