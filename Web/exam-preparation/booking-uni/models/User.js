95e29da3bab738bdac4d792b8addce68283f7766
JScodeKiddie <JScodeKiddie@gmail.com>
2023-01-23 21:02:46 +0100

    added second index for the username

--- Web/exam-preparation/booking-uni/models/User.js
+++ Web/exam-preparation/booking-uni/models/User.js
@@ -1,13 +1,31 @@
 const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema({
-    email: { type: String, required: [true, 'please enter email'] },
-    username: { type: String, required: [true, 'please enter username'] },
+    email: {
+        type: String,
+        required: [true, 'please enter email'],
+        unique: true,
+    },
+    username: {
+        type: String,
+        required: [true, 'please enter username'],
+        unique: true,
+    },
     hashedPassword: { type: String, required: [true, 'please enter password'] },
 });
 
 userSchema.index(
     { email: 1 },
+    {
+        collation: {
+            locale: 'en',
+            strength: 2,
+        },
+    }
+);
+
+userSchema.index(
+    { username: 1 },
     {
         collation: {
             locale: 'en',
