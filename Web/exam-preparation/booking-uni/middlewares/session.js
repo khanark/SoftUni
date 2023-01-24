834f3b68ea29d55b0b6f177a9c8b32017049915a
JScodeKiddie <JScodeKiddie@gmail.com>
2023-01-24 21:55:49 +0100

    create session middleware

---
+++ Web/exam-preparation/booking-uni/middlewares/session.js
@@ -0,0 +1,19 @@
+const { verifyToken } = require('../services/userService');
+
+module.exports = (req, res, next) => {
+    const token = req.cookies.token;
+    if (token) {
+        try {
+            const userData = verifyToken(token);
+            req.user = userData;
+            res.locals = {
+                user: req.user || undefined,
+            };
+        } catch (error) {
+            res.clearCookie('token');
+            res.redirect('/auth/login');
+            return;
+        }
+    }
+    next();
+};
