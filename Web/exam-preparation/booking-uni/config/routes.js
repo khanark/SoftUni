15eb55147e015b8176571a8169f29aa91d9eee2c
JScodeKiddie <JScodeKiddie@gmail.com>
2023-01-25 23:10:36 +0100

    added some routes

--- Web/exam-preparation/booking-uni/config/routes.js
+++ Web/exam-preparation/booking-uni/config/routes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const authControler = require('../controllers/auth');
 const homeControler = require('../controllers/home');
+const crudControler = require('../controllers/crud');
 const session = require('../middlewares/session');
 const trim = require('../middlewares/trim');
+const { isGuest } = require('../middlewares/guards');
 
 module.exports = app => {
     // adding express middlewares
@@ -16,4 +18,6 @@ module.exports = app => {
     // adding routes
     app.use('/auth', authControler);
     app.use('/', homeControler);
+    app.use('/home', homeControler);
+    app.use('/crud', crudControler);
 };
