ae5ab48eb11a6f7f91182dd0040af848c56cf3f6
JScodeKiddie <JScodeKiddie@gmail.com>
2023-01-25 23:11:15 +0100

    updated home controller

--- Web/exam-preparation/booking-uni/controllers/home.js
+++ Web/exam-preparation/booking-uni/controllers/home.js
@@ -1,7 +1,16 @@
+const { getSingle, getAll } = require('../services/hotelService');
+
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-    res.render('home');
+router.get('/', async (req, res) => {
+    const hotels = await getAll();
+    console.log(hotels);
+    res.render('home', { hotels });
+});
+
+router.get('/details/:id', async (req, res) => {
+    const hotel = await getSingle(req.params.id);
+    res.render('details', { hotel });
 });
 
 module.exports = router;
