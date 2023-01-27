764d8a9acf421e595d57435b32ca06c7a5e0f3a0
JScodeKiddie <JScodeKiddie@gmail.com>
2023-01-27 23:14:21 +0100

    updated guards

--- Web/exam-preparation/booking-uni/middlewares/guards.js
+++ Web/exam-preparation/booking-uni/middlewares/guards.js
@@ -1,5 +1,6 @@
 module.exports = {
     isGuest,
+    isOwner,
 };
 
 function isGuest() {
@@ -9,5 +10,17 @@ function isGuest() {
         } else {
             next();
         }
+    };
+}
+
+function isOwner(callback) {
+    return async (req, res, next) => {
+        const hotel = await callback(req.params.id);
+        const isOwner = hotel.owner == req.user?.id;
+        console.log(isOwner);
+        res.locals = {
+            isOwner,
+        };
+        !isOwner ? res.redirect('/auth/login') : next();
     };
 }
