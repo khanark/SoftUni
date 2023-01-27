b27df446d6f3c73c616bfd70fead489f0d750215
JScodeKiddie <JScodeKiddie@gmail.com>
2023-01-27 23:13:56 +0100

    added middlewares / guards to the crud routes

--- Web/exam-preparation/booking-uni/controllers/crud.js
+++ Web/exam-preparation/booking-uni/controllers/crud.js
@@ -2,8 +2,10 @@ const {
     createHotel,
     getSingle,
     updateHotel,
+    deleteSingle,
 } = require('../services/hotelService');
 const { bookHotel } = require('../services/userService');
+const { isOwner } = require('../middlewares/guards');
 const { parseError } = require('../util/utils');
 
 const router = require('express').Router();
@@ -25,12 +27,12 @@ router.post('/create', async (req, res) => {
     }
 });
 
-router.get('/edit/:id', async (req, res) => {
+router.get('/edit/:id', isOwner(getSingle), async (req, res) => {
     const hotel = await getSingle(req.params.id);
     res.render('edit', { hotel });
 });
 
-router.post('/edit/:id', async (req, res) => {
+router.post('/edit/:id', isOwner(getSingle), async (req, res) => {
     try {
         if (Object.values(req.body).some(val => val == '')) {
             throw new Error('Missing fields');
@@ -39,17 +41,34 @@ router.post('/edit/:id', async (req, res) => {
         res.redirect(`/details/${req.params.id}`);
     } catch (error) {
         const errors = parseError(error);
-        res.render('create', { errors });
+        const errorData = {
+            name: req.body.hotel,
+            city: req.body.city,
+            imageUrl: req.body.imgUrl,
+            freeRooms: req.body['free-rooms'],
+        };
+        res.render('edit', { errorData, errors });
     }
 });
 
 router.get('/book/:id', async (req, res) => {
     try {
+        const hotel = await getSingle(req.params.id);
+        if (hotel.owner == req.user.id) {
+            console.log('You cannot book a room in your own hotel.');
+            return res.redirect(`/details/${req.params.id}`);
+        }
         await bookHotel(req.params.id, req.user.id);
+        req.user.bookedHotels.push(hotel);
         res.redirect('/');
     } catch (error) {
         console.log(error);
     }
+});
+
+router.get('/delete/:id', isOwner(getSingle), async (req, res) => {
+    await deleteSingle(req.params.id);
+    res.redirect('/');
 });
 
 module.exports = router;
