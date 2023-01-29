7e50bd350b9d147a1f3b56b3e596c60916d77d20
JScodeKiddie <JScodeKiddie@gmail.com>
2023-01-29 22:11:04 +0100

    created User Schema

---
+++ Web/exam-preparation/book-talk/models/User.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        minLength: [4, 'Username must be atleast 4 characters long.'],
+    },
+    email: {
+        type: String,
+        minLength: [10, 'Email must be atleast 10 characters long.'],
+    },
+    password: {
+        type: String,
+        minLength: [3, 'Password must be atleast 3 characters long.'],
+    },
+});
+
+module.exports = model('User', userSchema);
