d63c1df3d094c468a602ee0f992de6332b1fa4e8
JScodeKiddie <JScodeKiddie@gmail.com>
2023-02-02 22:59:52 +0100

    added profile

--- Web/exam-preparation/book-talk/controllers/general.js
+++ Web/exam-preparation/book-talk/controllers/general.js
@@ -1,11 +1,34 @@
+const {
+  getBooks,
+  createBook,
+  getUserBooks,
+} = require('../services/bookService');
+const { getProfile } = require('../services/userService');
+
 const router = require('express').Router();
 
-router.get('/details/:id', (req, res) => {
-    res.render('details');
+router.get('/profile', async (req, res) => {
+  const user = await getProfile(req.user?.id);
+  const books = await getUserBooks(req.user?.id);
+  res.render('profile', { user, books });
 });
 
-router.get('/home/profile', (req, res) => {
-    res.render("profile");
+router.get('/create', (req, res) => {
+  res.render('create');
+});
+
+router.post('/create', async (req, res, next) => {
+  try {
+    await createBook(req.body, req.user.id);
+    res.redirect('/home/catalog');
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/catalog', async (req, res) => {
+  const books = await getBooks();
+  res.render('catalog', { books });
 });
 
 module.exports = router;
