fe155d1789a222d379cd5767cadf761c5c5de29d
JScodeKiddie <JScodeKiddie@gmail.com>
2023-02-02 23:00:40 +0100

    added setCookieAndRedirect function to the auth controller

--- Web/exam-preparation/book-talk/controllers/auth.js
+++ Web/exam-preparation/book-talk/controllers/auth.js
@@ -1,50 +1,48 @@
-const { register, login, verifyToken } = require('../services/userService');
-const { parseError } = require('../util/util');
+const { register, login } = require('../services/userService');
+const { setCookieAndRedirect } = require('../util/util');
 const router = require('express').Router();
 
 router.get('/register', (req, res) => {
-    res.render('register');
+  res.render('register');
 });
 
-router.post('register', async (req, res) => {
-    try {
-        const token = await register(req.body);
-        res.cookie('token', token);
-        res.redirect('/');
-    } catch (error) {
-        const errors = parseError(error);
-        res.render('register', {
-            body: {
-                email: req.body.email,
-                username: req.body.username,
-            },
-            errors,
-        });
+router.post('/register', async (req, res, next) => {
+  try {
+    if (Object.values(req.body).some(val => val == '')) {
+      throw new Error('Missing fields');
     }
+    if (req.body.password.length < 3) {
+      throw new Error('Invalid password: must be atleast 3 characters long');
+    }
+    if (req.body.password != req.body.repass) {
+      throw new Error("Passwords don't match");
+    }
+    const token = await register(req.body);
+    setCookieAndRedirect('/', res, token);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get('/login', (req, res) => {
-    res.render('login');
+  res.render('login');
 });
 
-router.post('/login', async (req, res) => {
-    try {
-        const token = await login(req.body);
-        res.cookie('token', token);
-    } catch (error) {
-        const errors = parseError(error);
-        res.render('register', {
-            body: {
-                email: req.body.email,
-            },
-            errors,
-        });
+router.post('/login', async (req, res, next) => {
+  try {
+    if (Object.values(req.body).some(val => val == '')) {
+      throw new Error('Missing fields');
     }
+    const token = await login(req.body);
+    setCookieAndRedirect('/', res, token);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get('/logout', (req, res) => {
-    res.clearCookie();
-    res.redirect('/');
+  res.clearCookie('token');
+  res.redirect('/');
 });
 
 module.exports = router;
