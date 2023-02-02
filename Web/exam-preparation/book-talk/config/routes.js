413e4e443b7a775e3d654fd4ee782483bafcdc94
JScodeKiddie <JScodeKiddie@gmail.com>
2023-02-02 22:59:24 +0100

    added more routes and fixed authorization

--- Web/exam-preparation/book-talk/config/routes.js
+++ Web/exam-preparation/book-talk/config/routes.js
@@ -1,23 +1,24 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-
-//routes
-const generalController = require('../controllers/general');
-const authController = require('../controllers/auth');
-const homeController = require('../controllers/home');
-const session = require('../middlewares/session');
-const trimmer = require('../middlewares/trimmer');
-const { isLogged } = require('../middlewares/guards');
+const { auth, general, home, book } = require('../controllers');
+const { guards, session, trimmer, error, preload } = require('../middlewares');
 
 module.exports = app => {
-    // express middlewares
-    app.use(express.urlencoded({ extended: true }));
-    app.use(express.static('static'));
-    app.use(cookieParser());
-    app.use(session());
-    app.use(trimmer());
+  app.use(session);
+  app.use(trimmer);
 
-    app.use('/', homeController);
-    app.use('/home', generalController);
-    app.use('/auth', isLogged(), authController);
+  app.use('/', home);
+  app.use('/home', guards.isLogged('catalog'), general);
+  app.use('/auth', auth);
+  app.use(
+    '/book/:id',
+    preload(),
+    guards.isLogged('details'),
+    guards.isOwner('details'),
+    book
+  );
+  app.all('*', (req, res) => {
+    res.render('404');
+  });
+
+  // global error handler has to be always last
+  app.use(error);
 };
