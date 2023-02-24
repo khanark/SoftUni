254b35ff9835655bdc6e4dea4bd5bfca65f5bad3
JScodeKiddie <JScodeKiddie@gmail.com>
2023-02-24 17:16:29 +0100

    delete custom error handler
    no longer needed

--- Web/React Project/REST Api/util/util.js
+++ Web/React Project/REST Api/util/util.js
@@ -1,18 +1,8 @@
 module.exports = {
-  handleError,
-  userViewModel,
+    userViewModel,
 };
 
-function handleError(response, statusCode, err) {
-  if (err.name == 'ValidationError') {
-    msg = Object.values(err.errors)[0].message;
-  } else {
-    msg = err.message;
-  }
-  response.status(statusCode).json({ err: msg, statusCode: statusCode });
-}
-
 function userViewModel(user, token) {
-  const { _id, username, email, isBanned, role, photo } = user;
-  return { _id, username, email, isBanned, role, photo, token };
+    const { _id, username, email, isBanned, role, photo } = user;
+    return { _id, username, email, isBanned, role, photo, token };
 }
