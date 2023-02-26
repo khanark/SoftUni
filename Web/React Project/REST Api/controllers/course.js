8fd2a8fc4dc29a2108c963c9282927a28b9d0680
JScodeKiddie <JScodeKiddie@gmail.com>
2023-02-26 11:29:45 +0100

    add validation for the user's role when working with course resources

--- Web/React Project/REST Api/controllers/course.js
+++ Web/React Project/REST Api/controllers/course.js
@@ -8,6 +8,7 @@ const {
 } = require('../services/course');
 const { isValidObjectId } = require('mongoose');
 const { verifyToken } = require('../services/user');
+const { validateUserRole } = require('../util/util');
 
 const router = require('express').Router();
 
@@ -72,7 +73,8 @@ router.delete('/:id', async (req, res, next) => {
                 cause: 404,
             });
         }
-        await verifyToken(req.headers);
+        const user = await verifyToken(req.headers);
+        validateUserRole(user.role, 'admin');
         await deleteCourse(id);
         res.status(200).json({
             message: `Course with id: ${id} successfully deleted`,
@@ -90,7 +92,8 @@ router.patch('/:id', async (req, res, next) => {
                 cause: 404,
             });
         }
-        await verifyToken(req.headers);
+        const user = await verifyToken(req.headers);
+        validateUserRole(user.role, 'mentor');
         const course = await startCourse(id);
         res.status(200).json(course);
     } catch (error) {
